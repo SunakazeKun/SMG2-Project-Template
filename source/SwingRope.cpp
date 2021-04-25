222fe22738ce55ef39b8cf86a27d0bca367a9dbc
SunakazeKun <nintendo.rpgforum@gmail.com>
2021-04-26 01:03:12 +0200

    woops

--- source/SwingRope.cpp
+++ source/SwingRope.cpp
@@ -6,18 +6,9 @@ SwingRope::SwingRope(const char* pName) : LiveActor(pName) {
 	_94 = 0.0f;
 	_98 = 0.0f;
 
-	_B4 = 1.0f;
-	_B8 = 0.0f;
-	_BC = 0.0f;
-	_C0 = 0.0f;
-	_C4 = 0.0f;
-	_C8 = 1.0f;
-	_CC = 0.0f;
-	_D0 = 0.0f;
-	_D4 = 0.0f;
-	_D8 = 0.0f;
-	_DC = 1.0f;
-	_E0 = 0.0f;
+	_B4[0] = 1.0f;
+	_B4[5] = 1.0f;
+	_B4[10] = 1.0f;
 
 	_E4 = 0.0f;
 	_E8 = 0.0f;
